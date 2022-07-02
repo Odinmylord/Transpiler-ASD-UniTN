09ab811fb50d16496692dd3df94d36aecad46ebf
G <rgermenia@gmail.com>
2022-07-02 21:02:43 +0200

    rimozione file di test

--- test.py
+++
@@ -1,8 +0,0 @@
-from Classes import Tree
-
-
-def isPlumpRec(T: Tree, l: int):
-    size = 1 if T.left is not None else 0 + 1 if T.right is not None else 0
-    if size < 2 ** (l - 1):
-        return False
-    return isPlumpRec(T.left, l + 1) and isPlumpRec(T.right, l + 1)
